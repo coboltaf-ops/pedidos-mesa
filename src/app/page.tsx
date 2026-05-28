9afb1aa19ced64afe57c4a12767772b72e4b8844
coboltaf-ops <coboltaf@gmail.com>
2026-05-28 13:44:12 -0500

    fix: Usar redirect server-side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Page() {
-  const router = useRouter()
-  
-  useEffect(() => {
-    router.push('/comidas/qr')
-  }, [router])
-  
-  return <div>Redirigiendo...</div>
+  redirect('/comidas/qr')
 }
